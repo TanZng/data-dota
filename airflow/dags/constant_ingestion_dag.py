59a22e35e9b7ed4b14282c263c98ef7980b738bd
cachoureze <sylvain.cachard@insa-lyon.fr>
2022-11-22 10:39:25 +0100

    wrangling region data to add city_name

--- airflow/dags/constant_ingestion_dag.py
+++ airflow/dags/constant_ingestion_dag.py
@@ -1,42 +1,72 @@
-import airflow
 import datetime
-# from pymongo import MongoClient
+import pymongo as py
+import json
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.postgres_operator import PostgresOperator
-import faker, requests, random, json
-
-# def get_match_ids(outputFile):
-
-#     # randomise a name
-#     name = faker.Faker().name()
-
-#     # write to file
-#     file = open(outputFile, "w+")
-#     file.write(json.dumps({'name':name}))
-    
-# def cache_data(uploadFile, tableName, postgresConnID):
-#     from airflow.hooks.postgres_hook import PostgresHook
-
-#     pg_hook = PostgresHook.get_hook(postgresConnID)
-#     pg_conn = pg_hook.get_conn()
-#     pg_hook.copy_expert("""COPY """+tableName+""" FROM stdin WITH CSV HEADER
-#                         DELIMITER as ',' """,
-#                         uploadFile)
-#     pg_conn.commit()
+import json
+from pandas import DataFrame
 
 def add_JSON_file_to_mongo(**context):
-    # print("***TEST*** attribut context in python function : " + context["region_file"])
-    # myclient = MongoClient("http://localhost::27017/")
-    # constant_db = myclient["constant_db"]
-    # region_coll = constant_db["region"]
-    # heros_coll = constant_db["heroes"]
-    region_json = open(context["region_file"], 'r+')
-    print(region_json)
-    # region_dict = 
-    # constant_db.region_coll.insert(region_dict)
+    myclient = py.MongoClient("mongodb://mongo:27017/")
+    constant_db = myclient["constant_db"]
+    region_coll = constant_db["region"]
+    heros_coll = constant_db["heroes"]
+
+    # Open and read local JSON region file
+    region_file = open(context["region_file"], 'r')
+    region_json = json.load(region_file)  
+    region_coll.insert_one(region_json)
+    region_file.close()
+
+    # Open and read local JSON heros file
+    heroes_file = open(context["heroes_file"], 'r')
+    heroes_json = json.load(heroes_file)
+    heros_coll.insert_one(heroes_json)
+    heroes_file.close()
+
+def add_city_attribute_to_region():
+    myclient = py.MongoClient("mongodb://mongo:27017/")
+    constant_db = myclient["constant_db"]
+    region_coll = constant_db["region"]
+
+    region_cursor = region_coll.find()
+    region_list = list(region_cursor)
+    region_list[0]["region"] = "region_name"
+    region_df = DataFrame(region_list).set_index("region")
+    region_df = region_df.transpose()
+    region_df = region_df.drop(labels="_id", axis=0)
+    region_df = region_df.drop(labels="13", axis=0)
+    city_list = [
+        ["1", "SAN FRANSISCO"],
+        ["2", "NEW YORK"],
+        ["3", "MADRID"],
+        ["5", "SINGAPORE"],
+        ["6", "DUBAI"],
+        ["7", "ALICE SPRINGS"],
+        ["8", "STOCKHOLM"],
+        ["9", "LIEZEN"],
+        ["10", "BRASILIA"],
+        ["11", "PRETORIA"],
+        ["12", "SHANGHAI"],
+        ["14", "SANTIAGO"],
+        ["15", "PUCALLPA"],
+        ["16", "NAGPUR"],
+        ["17", "GUANGDONG"],
+        ["18", "ZHEJIANG"],
+        ["19", "TOKYO"],
+        ["20", "WUHAN"],
+        ["25", "TIANJIN"],
+        ["37", "TAIPEI"],
+        ["38", "NEUQUEN"],
+    ]
+    city_df = DataFrame(city_list, columns=["region", "city_name"])
+    region_city_df = region_df.join(city_df.set_index("region"))    
+    region_city_coll = constant_db["region_city"]     
+    print(region_city_df)       
+    region_city_coll.insert_one(region_city_df.to_dict(orient="index"))
 
 default_args_dict = {
     'start_date': datetime.datetime(2022,10,14),
@@ -47,71 +77,33 @@ default_args_dict = {
 }
 
 first_dag = DAG(
-    dag_id='dota_init_dag',
+    dag_id='constant_ingestion_dag',
     default_args=default_args_dict,
     catchup=False,
 )
 
-# task_create_table = PostgresOperator(
-#     task_id='create_table',
-#     dag=first_dag,
-#     postgres_conn_id='postgres_default',
-#     sql='DROP TABLE IF EXISTS match_id; '\
-#         'CREATE TABLE match_id ('\
-#         '   match_id int,'\
-#         '   match_seq_num int,'\
-#         '   counter int);',
-#     trigger_rule='all_success',
-#     autocommit=True,
-# )
-
-# task_get_match_ids = PythonOperator(
-#     task_id='get_match_ids',
-#     dag=first_dag,
-#     python_callable=get_match_ids,
-#     op_kwargs={
-#         "outputFile": "./dags/tmp/name.txt"
-#     },
-# )
-
-# task_cache_data = PythonOperator(
-#     task_id='cache_data',
-#     dag=first_dag,
-#     python_callable=cache_data,
-#     op_kwargs={
-#         "outputFile": "./dags/tmp/name.txt"
-#     },
-# )
-
-# task_last = DummyOperator(
-#     task_id='end',
-#     dag=first_dag,
-#     trigger_rule='none_failed',
-#     depends_on_past=False,
-# )
-
 # download_constant = BashOperator(
 #     task_id = 'download_constant',
 #     dag = first_dag,
-#     bash_command = """curl -o /home/cachou/Documents/fundation_data_engineering/data-dota/data/region.json 
-#     https://raw.githubusercontent.com/odota/dotaconstants/master/build/region.json | 
-#     curl -o /home/cachou/Documents/fundation_data_engineering/data-dota/data/heroes.json 
-#     https://raw.githubusercontent.com/odota/dotaconstants/master/build/heroes.json"""
+#     bash_command = """curl https://raw.githubusercontent.com/odota/dotaconstants/master/build/region.json --output /opt/airflow/dags/data/region.json && curl https://raw.githubusercontent.com/odota/dotaconstants/master/build/heroes.json --output /opt/airflow/dags/data/heroes.json"""
 # )
 
-add_constant_to_mongo = PythonOperator(
-    task_id = "add_contant_to_mongo",
+# add_constant_to_mongo = PythonOperator(
+#     task_id = "add_constant_to_mongo",
+#     dag = first_dag,
+#     python_callable = add_JSON_file_to_mongo,
+#     op_kwargs={
+#         "region_file":"/opt/airflow/dags/data/region.json",
+#         "heroes_file":"/opt/airflow/dags/data/heroes.json"
+#     }
+# )
+
+add_city_attribute_to_region_task = PythonOperator(
+    task_id = "add_city_attribute_to_region",
     dag = first_dag,
-    python_callable = add_JSON_file_to_mongo,
-    op_kwargs={
-        "region_file":"/home/cachou/Documents/fundation_data_engineering/data-dota/data/region.json",
-        "heroes_file":"/home/cachou/Documents/fundation_data_engineering/data-dota/data/heroes.json"
-    }
+    python_callable = add_city_attribute_to_region,
 )
-# task_create_table >> task_get_match_ids >> task_cache_data >> task_last
-# download_constant
-add_constant_to_mongo
 
+add_city_attribute_to_region_task
 
-
-
+# download_constant >> add_constant_to_mongo >> 
