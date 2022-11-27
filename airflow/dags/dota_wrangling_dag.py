925f0b5b8b38cb91258eeba7505dc75b64fcba14
GenT322 <74548275+GenT322@users.noreply.github.com>
2022-11-27 17:51:07 +0100

    Update dota_wrangling_dag.py
    - Create sunlight table
    - Create mapping table between region, month to sunlight level
    - Create region table
    - Create match fact table

--- airflow/dags/dota_wrangling_dag.py
+++ airflow/dags/dota_wrangling_dag.py
@@ -76,10 +76,13 @@ def filter_data():
 
     regions = pd.DataFrame(constant_db.region_city.find()).set_index('region_id').drop('_id',axis=1)
 
+    #make all region 13 to region 12
+    match_details.loc[match_details['region']==13,'region'] = 12
+
     match_details = match_details.join(regions,on='region')
 
     match_details['sunlight'] = match_details.apply(\
-        lambda x: str(x['region_name'])+'_'+calendar.month_abbr[datetime.datetime.fromtimestamp(x['start_time']).month],axis=1)
+        lambda x: str(x['region_name']).replace(' ','_')+'_'+calendar.month_abbr[datetime.datetime.fromtimestamp(x['start_time']).month],axis=1)
 
     match_details = match_details.drop('city_name',axis=1).drop('region_name',axis=1)
 
@@ -87,6 +90,84 @@ def filter_data():
 
     for _, match_detail_item in match_details_dict.items():
         wrangling_db.match_details_filtered.insert_one(match_detail_item)
+
+def upload_region_to_dim_table():
+
+    import pymongo as pm, pandas as pd
+
+    from sqlalchemy import create_engine
+
+    conn_string = 'postgres://airflow:airflow@postgres:5432'
+    
+    db = create_engine(conn_string)
+    conn = db.connect()
+
+    mongo_client = pm.MongoClient("mongodb://mongo:27017/")
+    constant_db = mongo_client['constant_db']
+
+    regions = pd.DataFrame(constant_db.region_city.find()).drop('_id',axis=1).drop('city_name',axis=1)
+
+    regions.to_sql('region',con=conn,if_exists='replace',index=False)
+
+def create_sunlight_mapping_table():
+    import pymongo as pm, pandas as pd, numpy as np
+    import redis
+    from sqlalchemy import create_engine
+
+    conn_string = 'postgres://airflow:airflow@postgres:5432'
+    
+    db = create_engine(conn_string)
+    conn = db.connect()
+
+    redis_host = 'redis'
+    redis_port = '6379'
+
+    redis_client = redis.Redis(host=redis_host, port=redis_port, db=1)
+
+    mongo_client = pm.MongoClient("mongodb://mongo:27017/")
+    constant_db = mongo_client['constant_db']
+
+    regions = pd.DataFrame(constant_db.region_city.find()).set_index('region_id').drop('_id',axis=1)
+    regions['region_name'] = regions['region_name'].apply(lambda x: x.replace(' ','_'))
+
+    month_names = ['Dec', 'Feb', 'Jan', 'Jul', 'Jun', 'Mar', 'May', 'Nov', 'Oct', 'Sep']
+    monthly_sunlight = {i : 0 for i in month_names}
+
+    regional_monthly_sunlight = pd.DataFrame(columns = ['sunlight_id','sunlight_level_id'])
+
+    for _, region in regions.iterrows():
+        for month_name in month_names:
+            monthly_sunlight[month_name] = (float(redis_client.get(region['region_name']+'_'+month_name)))
+
+        average = sum(monthly_sunlight.values())/len(monthly_sunlight)
+        for month_name in month_names:
+            # x<0.85 = 1
+            # 0.85<x<0.95 = 2
+            # 0.95<x<1.05 = 3
+            # 1.05<x<1.15 = 4
+            # x>1.15 = 5
+            sunlight_level_id = np.ceil((min(max((monthly_sunlight[month_name]/average),0.85),1.3)-0.75)/0.1)
+            regional_monthly_sunlight.loc[len(regional_monthly_sunlight)] = \
+                [region['region_name']+'_'+month_name,sunlight_level_id]
+        
+    regional_monthly_sunlight.to_sql('sunlight_map',con=conn,if_exists='replace',index=False)
+
+def upload_match_to_fact_table():
+    import pymongo as pm, pandas as pd
+
+    from sqlalchemy import create_engine
+
+    conn_string = 'postgres://airflow:airflow@postgres:5432'
+    
+    db = create_engine(conn_string)
+    conn = db.connect()
+
+    mongo_client = pm.MongoClient("mongodb://mongo:27017/")
+    wrangling_db = mongo_client['wrangling_match_details']
+
+    match_details = pd.DataFrame(wrangling_db.match_details_filtered.find()).drop('players',axis=1).drop('_id',axis=1)
+
+    match_details.to_sql('match',con=conn,if_exists='replace',index=False)
 
 default_args_dict = {
     'start_date': datetime.datetime(2022,10,14),
@@ -113,7 +194,7 @@ task_create_match_table = PostgresOperator(
         '   human_players INT,'\
         '   start_time TIMESTAMP,'\
         '   region INT,'\
-        '   sunlight INT,'\
+        '   sunlight VARCHAR,'\
         '   comeback INT,'\
         '   stomp INT);',
     trigger_rule='all_success',
@@ -139,6 +220,35 @@ task_create_duration_table = PostgresOperator(
     autocommit=True,
 )
 
+task_create_sunlight_level_table = PostgresOperator(
+    task_id='create_sunlight_level_table',
+    dag=first_dag,
+    postgres_conn_id='postgres_default',
+    sql='DROP TABLE IF EXISTS sunlight_level; '\
+        'CREATE TABLE sunlight_level ('\
+        '   sunlight_level_id int,'\
+        '   name VARCHAR);'\
+        'INSERT INTO sunlight_level VALUES(1,\'VERY DARK\'),'\
+        '   (2, \'DARK\'),'\
+        '   (3, \'NORMAL\'),'\
+        '   (4, \'BRIGHT\'),'\
+        '   (5, \'VERY BRIGHT\');',
+    trigger_rule='all_success',
+    autocommit=True,
+)
+
+task_upload_region_to_dim_table = PythonOperator(
+    task_id='upload_region_to_dim_table',
+    dag=first_dag,
+    python_callable=upload_region_to_dim_table,
+)
+
+task_create_sunlight_mapping_table = PythonOperator(
+    task_id='create_sunlight_mapping_table',
+    dag=first_dag,
+    python_callable=create_sunlight_mapping_table,
+)
+
 task_mock_injestions = PythonOperator(
     task_id='mock_injestions',
     dag=first_dag,
@@ -160,6 +270,12 @@ task_filter_data = PythonOperator(
     python_callable=filter_data,
 )
 
+task_upload_match_to_fact_table = PythonOperator(
+    task_id='upload_match_to_fact_table',
+    dag=first_dag,
+    python_callable=upload_match_to_fact_table,
+)
+
 task_last = DummyOperator(
     task_id='end',
     dag=first_dag,
@@ -167,4 +283,6 @@ task_last = DummyOperator(
     depends_on_past=False,
 )
 
-[task_mock_injestions, task_create_match_table >> task_create_duration_table] >> task_select_attributes >> task_filter_data >> task_last
+[task_mock_injestions] >> task_select_attributes >> task_filter_data >> task_upload_match_to_fact_table >> task_last
+
+task_create_match_table >> [task_create_duration_table, task_upload_region_to_dim_table, task_create_sunlight_level_table >> task_create_sunlight_mapping_table] >> task_filter_data
