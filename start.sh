082dae15a1e1a332d0251c23714292325b0a5e34
Tan 🔥 <tania.zuniga@pipefy.com>
2022-11-27 12:49:10 +0000

    update start script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+FILE=./.env
+if [ ! -f "$FILE" ]; then
 sh -c "
 cat <<EOF >>./.env
 _AIRFLOW_WWW_USER_USERNAME=airflow
@@ -9,23 +11,27 @@ AIRFLOW_GID=0
 _PIP_ADDITIONAL_REQUIREMENTS=xlsx2csv==0.7.8 faker==8.12.1 apache-airflow-providers-mongo==2.3.1 apache-airflow-providers-docker==2.1.0
 EOF
 "
+fi
 
+FILE=./docker-socket-proxy.yaml
+if [ ! -f "$FILE" ]; then
 sh -c "
 cat <<EOF >>./docker-socket-proxy.yaml
 services:
   docker-socket-proxy:
     image: tecnativa/docker-socket-proxy:0.1.1
     environment:
-      CONTAINERS: 1
-      IMAGES: 1
-      AUTH: 1
-      POST: 1
+    CONTAINERS: 1
+    IMAGES: 1
+    AUTH: 1
+    POST: 1
     restart: always
     privileged: true
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock:ro
 EOF
 "
+fi
 
 docker build -f ./avg_sunlight_by_region/Dockerfile -t avg_sunlight_by_region ./avg_sunlight_by_region
 
